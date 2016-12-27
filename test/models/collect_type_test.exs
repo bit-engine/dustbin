dcced02073d585b6aa5ff40bde57aaee0844e728
Saúl Cabrera <saulecabrera@gmail.com>
2016-12-26 19:20:34 -0500

    added test for id to be uuid

--- test/models/collect_type_test.exs
+++ test/models/collect_type_test.exs
@@ -7,13 +7,24 @@ defmodule Dustbin.CollectTypeTest do
   @valid_attrs %{name: "Trash"}
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
+  test "changeset is valid when all required attributes are present" do
     changeset = CollectType.changeset(%CollectType{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
+  test "changeset is invalid when no attributes are provided" do
     changeset = CollectType.changeset(%CollectType{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "changeset is invalid when empty required attributes are provided" do
+    changeset = CollectType.changeset(%CollectType{}, %{name: ""})
+    refute changeset.valid?
+  end
+
+  test "CollectType id is type uuid" do
+    changeset = CollectType.changeset(%CollectType{}, @valid_attrs)
+    {:ok, collect_type} = Repo.insert changeset
+    <<_::64, ?-, _::32, ?-, _::32, ?-, _::32, ?-, _::96>> = collect_type.id
   end
 end
