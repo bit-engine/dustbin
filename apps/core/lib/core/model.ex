fb959da5bd57120389d21504f48a3c9bebdca5f9
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-20 11:27:06 -0500

    added model module

---
+++ apps/core/lib/core/model.ex
@@ -0,0 +1,14 @@
+defmodule Core.Model do
+
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      import Ecto.Query
+
+      alias Core.{
+        CollectType 
+      }
+    end
+  end
+end
