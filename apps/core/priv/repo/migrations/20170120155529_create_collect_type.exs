eb33aa58a6e5522e4e2766b6c527c3e817268b3b
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-20 16:57:38 -0500

    added collect_type migration

--- apps/core/priv/repo/migrations/20170120155529_create_collect_type.exs
+++ apps/core/priv/repo/migrations/20170120155529_create_collect_type.exs
@@ -2,6 +2,13 @@ defmodule Core.Repo.Migrations.CreateCollectType do
   use Ecto.Migration
 
   def change do
+    create table(:collect_types, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :type, :string
+      
+      timestamps()
+    end
 
+    create unique_index(:collect_types, [:type])
   end
 end
