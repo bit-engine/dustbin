938629aa82bab012d4df233cf71e34e62c136d2b
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-20 11:00:43 -0500

    fixed typo in migration name

--- apps/core/priv/repo/migrations/20170120155529_create_collec_type.exs
+++
@@ -1,7 +0,0 @@
-defmodule Core.Repo.Migrations.CreateCollecType do
-  use Ecto.Migration
-
-  def change do
-
-  end
-end
---
+++ apps/core/priv/repo/migrations/20170120155529_create_collect_type.exs
@@ -0,0 +1,7 @@
+defmodule Core.Repo.Migrations.CreateCollectType do
+  use Ecto.Migration
+
+  def change do
+
+  end
+end
