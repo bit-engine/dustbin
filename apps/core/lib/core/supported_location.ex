a7d3ededb2e802d20c1d226476ef4dde87d045ce
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-25 20:30:26 -0500

    Supported Locations
    Added supported_locations migration and schema

---
+++ apps/core/lib/core/supported_location.ex
@@ -0,0 +1,21 @@
+defmodule Core.SupportedLocation do
+  use Core.Model
+
+  @fields [:city, :province_or_state, :country, :country_code] 
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "supported_locations" do
+    field :city, :string
+    field :province_or_state, :string
+    field :country, :string
+    field :country_code, :string
+    
+    timestamps()
+  end
+
+  def changeset(supported_location, params \\ %{}) do
+    supported_location
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+  end
+end
