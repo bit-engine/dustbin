4aeb87a554a1869ff4958fd1c03796a0b3334108
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-26 21:00:27 -0500

    added collection schedule migration and table

---
+++ apps/core/priv/repo/migrations/20170127001541_add_collection_schedule.exs
@@ -0,0 +1,16 @@
+defmodule Core.Repo.Migrations.AddCollectionSchedules do
+  use Ecto.Migration
+
+  def change do
+    create table(:collection_schedules, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :scheduled_date, :date
+      add :supported_location_id, references(:supported_locations, type: :binary_id)
+      add :collect_type_id, references(:collect_types, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:collection_schedules, [:supported_location_id, :collect_type_id])
+  end
+end
