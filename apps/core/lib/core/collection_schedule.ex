5a881616653a2361115e82fbe8429041c47c4fdd
Saúl Cabrera <saulecabrera@gmail.com>
2017-01-31 21:18:11 -0500

    First compiling version of upcoming function
    Added Timex
    Added CollectionSchedule.upcoming/1 to retrieve next schedule

--- apps/core/lib/core/collection_schedule.ex
+++ apps/core/lib/core/collection_schedule.ex
@@ -1,5 +1,7 @@
 defmodule Core.CollectionSchedule do
   use Core.Model
+  import Ecto, only: [assoc: 2]
+  use Timex
 
   @fields [:scheduled_date]
 
@@ -15,5 +17,16 @@ defmodule Core.CollectionSchedule do
     collection_schedule
     |> cast(params, @fields)
     |> validate_required(@fields)
+  end
+
+  def upcoming(%SupportedLocation{} = location) do
+    tomorrow = Timex.now(location.timezone) |> Timex.to_date |> Timex.shift(days: 1)
+    location_schedules = assoc(location, :collection_schedules)
+    
+    upcoming = from sch in location_schedules,
+      where: sch.scheduled_date == ^tomorrow,
+      preload: [:collect_type]
+
+    Repo.all upcoming
   end
 end
