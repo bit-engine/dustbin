fcf4ac8333d4881801d1fdace7aa566627c271e8
Saúl Cabrera <saulecabrera@gmail.com>
2017-02-06 20:34:31 -0500

    Added Core.supported_locations/1

--- apps/core/test/core_test.exs
+++ apps/core/test/core_test.exs
@@ -1,2 +1,12 @@
 defmodule CoreTest do
+  use Core.Case
+
+  test "supported_locations/0 returns all the existent locations" do
+    insert(:supported_location, city: "Montreal", timezone: "America/Toronto")
+    insert(:supported_location, city: "Toronto", timezone: "America/Toronto")
+    insert(:supported_location, city: "Buenos Aires", timezone: "America/Argentina/Buenos_Aires")
+
+    result = Core.supported_locations
+    assert length(result) == 3
+  end
 end
