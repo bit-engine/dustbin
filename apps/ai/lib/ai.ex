e29e6a059f317d908fd46d39234117fdafb700e8
Saúl Cabrera <saulecabrera@gmail.com>
2017-02-23 19:18:40 -0500

    Add initial AI app

---
+++ apps/ai/lib/ai.ex
@@ -0,0 +1,18 @@
+defmodule AI do
+  @moduledoc """
+  Documentation for AI.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AI.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
