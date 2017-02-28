f94af3f13557a0f52f50c9ebdeff9dcd5521891c
Saúl Cabrera <saulecabrera@gmail.com>
2017-02-27 21:00:09 -0500

    Added initial tests
    Are tests stepping into each other an issue?

--- apps/ai/test/ai_test.exs
+++ apps/ai/test/ai_test.exs
@@ -1,8 +1,8 @@
 defmodule AITest do
   use ExUnit.Case
-  doctest AI
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "AI should create a new sesssion for a sender that doesn't has one" do
+    sender = "12345"
+    {session_id, %{context: %{}, fbid: ^sender}} = AI.create_session sender
   end
 end
