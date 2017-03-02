f2fcbe9de1fae93202e610bb5f39c71d0ada4aea
Saúl Cabrera <saulecabrera@gmail.com>
2017-03-01 21:27:09 -0500

    Added testing for AI.create_session
    Added test for creating and retrieving a session.
    Added assert_down private function to assert on down processes before
    running next test case
    Added alias for test test --no-start to avoid starting supervision on
    test mode

--- apps/ai/test/ai_test.exs
+++ apps/ai/test/ai_test.exs
@@ -1,8 +1,29 @@
 defmodule AITest do
   use ExUnit.Case
 
+  setup do
+    {:ok, ai} = AI.start_link()
+
+    on_exit fn ->
+      assert_down ai
+    end
+  end
+
   test "AI should create a new sesssion for a sender that doesn't has one" do
     sender = "12345"
     {session_id, %{context: %{}, fbid: ^sender}} = AI.create_session sender
+  end
+
+  test "AI should not create a session when a sender has a existing valid one" do
+    state = %{"some-session-id" => %{fbid: 123, context: %{}}}
+    {:reply, result, new_state} = AI.handle_call({:create_session, 123}, AI, state)
+
+    assert result == {"some-session-id", %{fbid: 123, context: %{}}}
+    assert new_state == state
+  end 
+
+  defp assert_down(pid) do
+    ref = Process.monitor(pid) 
+    assert_receive {:DOWN, ^ref, _, _, _}
   end
 end
