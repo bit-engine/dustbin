60ac1b167b089580eb47786cd4a3315327281a9b
Saúl Cabrera <saulecabrera@gmail.com>
2017-04-30 20:03:20 -0400

    fix repo config in receiver

--- apps/receiver/config/config.exs
+++ apps/receiver/config/config.exs
@@ -20,6 +20,8 @@ config :logger, :console,
 
 config :receiver, fb_verify_token: System.get_env("FB_VERIFY_TOKEN")
 
+config :receiver, ecto_repos: []
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
