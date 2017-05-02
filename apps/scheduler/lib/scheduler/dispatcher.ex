29010e5699bbb1880d59df444d754d7531eb348f
Saúl Cabrera <saulecabrera@gmail.com>
2017-05-01 20:44:51 -0400

    added tomorrow/1 helper

--- apps/scheduler/lib/scheduler/dispatcher.ex
+++ apps/scheduler/lib/scheduler/dispatcher.ex
@@ -3,7 +3,7 @@ defmodule Scheduler.Dispatcher do
   def notify(location = %Core.SupportedLocation{}) do
     collects = Core.upcoming_collects(location)
     if length(collects > 0) do
-      initial_message = "[#{location.city}, #{location.country}] - Collects for tomorrow: \n"
+      initial_message = "[#{location.city}, #{location.country}] - Collects for #{tomorrow(location.timezone)}: \n"
       notification_message = compose_notification_message(collects, initial_message)
       ExTwitter.update notification_message
     end
@@ -12,5 +12,11 @@ defmodule Scheduler.Dispatcher do
   defp compose_notification_message([], msg), do: msg
   defp compose_notification_message([h | t], msg) do
     compose_notification_message(t, msg <> "#{ - h.collect_type.type } \n") 
+  end
+
+  defp tomorrow(timezone) do
+    Timex.now(timezone)
+    |> Timex.to_date
+    |> Timex.shift(days: 1)
   end
 end
