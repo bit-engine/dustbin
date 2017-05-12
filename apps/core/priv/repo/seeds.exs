4fbbd117c28abdf0fe337e8b72c76a48764d3bfb
Saúl Cabrera <saulecabrera@gmail.com>
2017-05-11 20:07:53 -0400

    added better collect type seeding

--- apps/core/priv/repo/seeds.exs
+++ apps/core/priv/repo/seeds.exs
@@ -10,10 +10,7 @@ defmodule Core.DBSeeder do
 
   @collect_types ["Recyclable Material", "Garbage", "Bulky Garbage", "Green Waste", "Natural Pine Tree", "Hazardous Household Waste", "Wood", "Electronic & Computer Equipment"]
 
-  Enum.each(@collect_types, fn type -> 
-    seed CollectType, fn collect_type ->
-      %{collect_type | type: type}
-    end
-  end)
+  # seed collect types
+  for type <- @collect_types, do: seed CollectType, fn collect_type -> %{collect_type | type: type} end
 
 end
