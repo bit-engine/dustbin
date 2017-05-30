d79f75e985d864083959a626a492820b4db6cb4c
Saúl Cabrera <saulecabrera@gmail.com>
2017-05-30 15:22:44 -0400

    Code indentation

--- apps/receiver/web/controllers/messenger_controller.ex
+++ apps/receiver/web/controllers/messenger_controller.ex
@@ -3,29 +3,29 @@ defmodule Receiver.MessengerController do
   @moduledoc false
   @fb_verify_token Application.get_env(:receiver, :fb_verify_token)
 
-	use Receiver.Web, :controller
+  use Receiver.Web, :controller
 
   require Logger
 
 
-	def webhook(conn, %{"hub.verify_token" => token, "hub.mode" => "subscribe", "hub.challenge" => challenge}) do
-		if token == @fb_verify_token do
+  def webhook(conn, %{"hub.verify_token" => token, "hub.mode" => "subscribe", "hub.challenge" => challenge}) do
+    if token == @fb_verify_token do
       Logger.info "Webhook successful"
-			send_resp(conn, 200, challenge)
-		else
+      send_resp(conn, 200, challenge)
+    else
       Logger.error "Webhook validation failed"
-		  send_resp(conn, 403, "Not Allowed")
-		end
-	end
+      send_resp(conn, 403, "Not Allowed")
+    end
+  end
 
   def webhook(conn, _) do
     Logger.error "Incorrect parameters supplied to webhook"
-		send_resp(conn, 403, "Incorrect parameters")
+    send_resp(conn, 403, "Incorrect parameters")
   end
 
   def receive(conn, %{"object" => "page", "entry" => entry}) do
     Enum.each(entry, fn %{"messaging" => messaging} -> 
-      process_entry_messages(messaging)
+     process_entry_messages(messaging)
     end)
     send_resp(conn, 200, "")
   end
