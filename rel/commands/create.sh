7e2c79ea5034f98a997b74e6424fa3949e9f0a34
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-25 18:52:04 -0400

    Attempt 3 on create script

--- rel/commands/create.sh
+++ rel/commands/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-psql -U postgres -c "CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';" &&
+sudo -u postgres bash -c "psql -c \"CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';\"" &&
 createdb "${DUSTBIN_DB_NAME}" &&
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};" &&
+sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};\"" &&
 echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_SB_USERNAME}' have been created."
 
