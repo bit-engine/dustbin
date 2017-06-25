d5395e70fe3e2d6e7aab4e91792eb548bd5b1b26
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-25 19:02:17 -0400

    Fix script in create, again

--- rel/commands/create.sh
+++ rel/commands/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo -u postgres bash -c "psql -c \"CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';\"" &&
+sudo -u postgres psql -c "CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';" &&
 createdb "${DUSTBIN_DB_NAME}" &&
-sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};\"" &&
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};" &&
 echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_SB_USERNAME}' have been created."
 
