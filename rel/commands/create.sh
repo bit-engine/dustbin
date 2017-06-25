1f986d24bc8924d68184d988305d132ca9fe3ae2
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-25 19:59:33 -0400

    fix typo in create script

--- rel/commands/create.sh
+++ rel/commands/create.sh
@@ -3,5 +3,5 @@
 sudo -u postgres psql -c "CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';" &&
 sudo -u postgres createdb "${DUSTBIN_DB_NAME}" &&
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};" &&
-echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_SB_USERNAME}' have been created."
+echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_DB_USERNAME}' have been created."
 
