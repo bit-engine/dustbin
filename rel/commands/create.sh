7507a7e5939fcc0688f4de24cb8f34bf7b1f3a56
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-25 18:11:05 -0400

    Attempt 1 to create db

--- rel/commands/create.sh
+++ rel/commands/create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo su - postgres
 psql -c "CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';" &&
 createdb "${DUSTBIN_DB_NAME}" &&
 psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};" &&
