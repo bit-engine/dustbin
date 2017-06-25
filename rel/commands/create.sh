a50e16e8d76e7650dda6e08311fef563a79c1be0
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-25 19:50:01 -0400

    Added sudo for create db

--- rel/commands/create.sh
+++ rel/commands/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo -u postgres psql -c "CREATE USER ${DUSTBIN_DB_USERNAME} WITH PASSWORD '${DUSTBIN_DB_PASSWORD}';" &&
-createdb "${DUSTBIN_DB_NAME}" &&
+sudo -u postgres createdb "${DUSTBIN_DB_NAME}" &&
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DUSTBIN_DB_NAME} to ${DUSTBIN_DB_USERNAME};" &&
 echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_SB_USERNAME}' have been created."
 
