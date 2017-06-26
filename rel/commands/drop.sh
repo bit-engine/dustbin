2ef07a10391a8bd5802baeb3ba048672560571a7
Saúl Cabrera <saulecabrera@gmail.com>
2017-06-26 10:28:06 -0400

    Scope postgres user for drop command

--- rel/commands/drop.sh
+++ rel/commands/drop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dropdb "${DUSTBIN_DB_NAME}" &&
-dropuser "${DUSTBIN_DB_USERNAME}" &&
+sudo -u postgres dropdb "${DUSTBIN_DB_NAME}" &&
+sudo -u postgres dropuser "${DUSTBIN_DB_USERNAME}" &&
 echo "The database '${DUSTBIN_DB_NAME}' and role '${DUSTBIN_DB_USERNAME}' have been dropped."
